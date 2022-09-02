4cb9ae75a8c2b90ec26dda72561b8ea65ff54a26
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-02 23:58:49 +0600

    insert category name to the one line default category menu.

--- js/script.js
+++ js/script.js
@@ -7,15 +7,22 @@ const loadAllNews = async () => {
   const data = await res.json();
   return data;
 };
+//// ----------------------------------------------
+//// Show All the new category to the Category menu
+//// ----------------------------------------------
 const showAllCategories = async () => {
   const data = await loadAllNews();
   const allCategories = data.data.news_category;
-  console.log(allCategories);
+  // console.log(allCategories);
   // Access individual category
   allCategories.forEach((category) => {
     const { category_name, category_id } = category;
-    console.log(category_name, category_id);
-    //insert category to the online default menu
+    //insert category name to the one line default category menu
+    const categoryMenu = document.getElementById("category-menu");
+    const nameItem = document.createElement("p");
+    nameItem.classList.add("px-4", "py-3", "mb-0", "text-center");
+    nameItem.innerText = category_name;
+    categoryMenu.appendChild(nameItem);
   });
 };
 showAllCategories();
