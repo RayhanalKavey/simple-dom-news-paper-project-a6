21cdabb00890fb02e9b6f7a4b20f905877b2273e
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-03 03:21:10 +0600

    create card and append to the news feed field.

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const showAllCategories = async () => {
     const nameItem = document.createElement("div");
     // console.log(nameItem);
     nameItem.innerHTML = `
-            <p class="news px-4 py-3 mb-0 text-center" onclick="newInCategory(${category_id})" >  ${category_name}</p> 
+            <p class="news px-4 py-3 mb-0 text-center" onclick="newInCategory('${category_id}')" >  ${category_name}</p> 
 
     `;
     categoryMenu.appendChild(nameItem);
@@ -32,12 +32,64 @@ showAllCategories();
 //// --------------------------
 //// load the news in a category
 //// --------------------------
-const newInCategory = async (id) => {
+const newInCategory = (id) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => newsFeed(data.data));
 };
 //// ----------------------------------------------
 //// Show All the new details in the new feed
 //// ----------------------------------------------
+const newsFeed = (newsDetails) => {
+  const newsFeedField = document.getElementById("news-feed");
+  newsFeedField.innerHTML = "";
+  newsDetails.forEach((news) => {
+    // console.log(news);
+    const {
+      _id,
+      author,
+      title,
+      image_url,
+      thumbnail_url,
+      details,
+      others_info,
+      rating,
+      total_view,
+    } = news;
+    const { name, published_date, img } = author;
+    //Insert news details in the news feed
+    const newsDiv = document.createElement("div");
+    newsDiv.classList.add("col");
+    newsDiv.innerHTML = `
+ <div class="card flex-row">
+            <img src="${image_url}" class="w-25 h-full" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title text-dark">${title}</h5>
+              <p class="card-text">
+               ${
+                 details.length > 300 ? details.slice(0, 300) + " ..." : details
+               }
+              </p>
+              <p class="card-text">
+             <div class="d-flex justify-content-between align-items-end">
+              <div class="d-flex align-items-center">
+                <div class="me-2">  <img class="news-author" src="${img}" alt=""></div>
+                <div>
+                <div>${name}</div>
+                <div>${published_date}</div>
+                </div>
+              </div>
+              <div>${total_view ? total_view : "No"} view</div>
+              <div>rating</div>
+              <button>details</button>
+            </div>
+              </p>
+            </div>
+          </div>
+  
+  `;
+    newsFeedField.appendChild(newsDiv);
+  });
+};
